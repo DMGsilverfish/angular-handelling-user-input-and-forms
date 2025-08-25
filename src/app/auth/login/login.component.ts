134ae626293ade480c41438856103021354222f4
Daniel <danielgibson.pe@gmail.com>
2025-08-25 10:34:30 +0200

    Validating form status, v244

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +8,16 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    if (!formData.valid) {
+      return;
+    }
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+
+
+    console.log(formData.form);
+    console.log(enteredEmail, enteredPassword);
+  }
+}
